c2837f425e707a85299e2bb8bd622ab40e8f0285
Subarna Sutradhar <subarna04work@gmail.com>
2022-03-13 11:17:02 +0530

    Create tut5.cpp

---
+++ tut5.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2,sum;
+    cout<<"WTF\n";
+    cout<<"haha"<<endl;
+    cout<<"Enter the number: ";
+    cin>>num1;
+    cout<<"Enter the number: ";
+    cin>>num2;
+
+    sum = num1 + num2;
+
+    cout<<"The sum is : "<<sum;
+    return 0;
+}
