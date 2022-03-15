632ce9e3452bb11f7011c2564a626b2622dc1fb5
Subarna Sutradhar <subarna04work@gmail.com>
2022-03-15 19:18:04 +0530

    Update tut7.cpp

--- tut7.cpp
+++ tut7.cpp
@@ -4,16 +4,25 @@ using namespace std;
 int c =45;
 
 int main(){
-    int a,b,c;
+    // int a,b,c;
 
-    cout<<"Enter the value of a"<<endl;
-    cin>>a;
-    cout<<"Enter the value of b"<<endl;
-    cin>>b;
-    c = a+b;
-    cout<<"The sum is "<<c<<endl;
-    cout<<"global c is "<<::c;
-    //here :: is scope reolution operator
-    //used to print the global value
+    // cout<<"Enter the value of a"<<endl;
+    // cin>>a;
+    // cout<<"Enter the value of b"<<endl;
+    // cin>>b;
+    // c = a+b;
+    // cout<<"The sum is "<<c<<endl;
+    // cout<<"global c is "<<::c;
+    // //here :: is scope reolution operator
+    // //used to print the global value
+
+    float d = 34.4F; //for float
+    long double e = 34.4L;
+
+    cout<<"The size of 34.4 is"<<sizeof(34.4)<<endl;
+    cout<<"The size of 34.4 is"<<sizeof(34.4f)<<endl;
+    cout<<"The size of 34.4 is"<<sizeof(34.4F)<<endl;
+    cout<<"The size of 34.4 is"<<sizeof(34.4l)<<endl;
+    cout<<"The size of 34.4 is"<<sizeof(34.4L)<<endl;
     return 0;
 }
