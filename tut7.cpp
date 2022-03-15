577d3b1163dd8c0ae939b796c734e4ae47a72bfc
Subarna Sutradhar <subarna04work@gmail.com>
2022-03-15 19:21:49 +0530

    Update tut7.cpp

--- tut7.cpp
+++ tut7.cpp
@@ -16,13 +16,19 @@ int main(){
     // //here :: is scope reolution operator
     // //used to print the global value
 
-    float d = 34.4F; //for float
-    long double e = 34.4L;
+    // float d = 34.4F; //for float
+    // long double e = 34.4L;
 
-    cout<<"The size of 34.4 is"<<sizeof(34.4)<<endl;
-    cout<<"The size of 34.4 is"<<sizeof(34.4f)<<endl;
-    cout<<"The size of 34.4 is"<<sizeof(34.4F)<<endl;
-    cout<<"The size of 34.4 is"<<sizeof(34.4l)<<endl;
-    cout<<"The size of 34.4 is"<<sizeof(34.4L)<<endl;
+    // cout<<"The size of 34.4 is"<<sizeof(34.4)<<endl;
+    // cout<<"The size of 34.4 is"<<sizeof(34.4f)<<endl;
+    // cout<<"The size of 34.4 is"<<sizeof(34.4F)<<endl;
+    // cout<<"The size of 34.4 is"<<sizeof(34.4l)<<endl;
+    // cout<<"The size of 34.4 is"<<sizeof(34.4L)<<endl;
+
+    //reference variable
+    float x = 455;
+    float & y = x;//y is reference to x
+    cout<<x<<endl;
+    cout<<y<<endl;
     return 0;
 }
