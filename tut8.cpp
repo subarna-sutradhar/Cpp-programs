813f8c6933c144b40721db94384d1a620a6012c6
Subarna Sutradhar <subarna04work@gmail.com>
2022-03-19 18:09:10 +0530

    Create tut8.cpp

---
+++ tut8.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int a =34;
+    // cout<<"The value of a was : "<<a;
+    // a=45;
+    // cout<<"The value of a is : "<<a;
+    // constant in cpp
+    // int a = 3.11;
+    const a = 3;
+    cout<<"The value of a was : "<<a;
+    a=45;
+    cout<<"The value of a is : "<<a;
+
+
+    return 0;
+}
