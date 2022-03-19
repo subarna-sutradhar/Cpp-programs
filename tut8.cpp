6bb33775914e31c2015446d290cccb082dc46d06
Subarna Sutradhar <subarna04work@gmail.com>
2022-03-19 18:21:43 +0530

    Update tut8.cpp

--- tut8.cpp
+++ tut8.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<iomanip>
 using namespace std;
 
 int main(){
@@ -8,11 +9,25 @@ int main(){
     // cout<<"The value of a is : "<<a;
     // constant in cpp
     // int a = 3.11;
-    const a = 3;
-    cout<<"The value of a was : "<<a;
-    a=45;
-    cout<<"The value of a is : "<<a;
+    // const int a = 3;
+    // cout<<"The value of a was : "<<a<<endl;
+    // a=45;//error due to const a
+    // cout<<"The value of a is : "<<a<<endl;
+    // cout<<"The value of a is : "<<a<<endl;
 
+    //manipulators in c
+    // int a =3, b = 45, c = 1234;
+    // cout<<"The value of a without setw is : "<<a<<endl;
+    // cout<<"The value of b without setw is : "<<b<<endl;
+    // cout<<"The value of c without setw is : "<<c<<endl;
 
+    // cout<<"The value of a is : "<<setw(4)<<a<<endl;
+    // cout<<"The value of b is : "<<setw(4)<<b<<endl;
+    // cout<<"The value of c is : "<<setw(4)<<c<<endl;
+
+    //operator precedence
+    int a = 3, b = 4;
+    int c = a*5 + b;
+    cout<<c;
     return 0;
 }
